3ae5488356a4950324e61f74cf434f36dbd8cdf9
Sam <stuffnbotany@gmail.com>
2018-09-20 12:57:18 +0200

    Filling the void

---
+++ template/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">{{#router}}
+    <router-view/>
+    {{else}}
+    <HelloWorld/>
+    {{/router}}
+  </div>
+</template>
+
+<script>
+{{#unless router}}
+import HelloWorld from './components/HelloWorld'
+
+{{/unless}}
+export default {
+  name: 'App'{{#router}}{{else}},
+  components: {
+    HelloWorld
+  }{{/router}},
+  props: [],
+  // created(){},
+  // mounted(){},
+  // updated(){},
+  // activated(){}
+  computed: {},
+  watch: {},
+  methods: {},
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/css/reset.css';
+@import './assets/css/functional.css';
+
+#app {
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  font-family: Montserrat;
+}
+</style>
